3ddd610ae447f413dbe739c3a84c7b4ca29ab326
kushanz <kushanshanuka@yahoo.com>
2022-09-30 13:39:03 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { of, map, filter } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,21 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  list = [
+    { name: 'kushan', age: 24, marks: 67 },
+    { name: 'kamal', age: 27, marks: 39 },
+    { name: 'nimal', age: 34, marks: 57 },
+    { name: 'ann', age: 44, marks: 68 },
+  ];
+  users$ = of(this.list);
+  usernames$ = this.users$.pipe(map((users) => users.map((user) => user.name)));
+  passedlist$ = this.users$.pipe(
+    filter((users) => users.every((user) => user.marks > 50))
+  );
+  // passed = this.users$.pipe(map((users) => filter((user) => )))
+  ngOnInit() {
+    this.users$.subscribe((d) => {
+      console.log(d);
+    });
+  }
 }
